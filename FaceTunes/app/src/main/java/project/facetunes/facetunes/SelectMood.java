3f8cb3394366244e2ce5021825414b5d4884c570
Sahil Modi <sm34524@gmail.com>
2017-10-11 20:00:34 -0500

    Added result activity framework

--- FaceTunes/app/src/main/java/project/facetunes/facetunes/SelectMood.java
+++ FaceTunes/app/src/main/java/project/facetunes/facetunes/SelectMood.java
@@ -1,9 +1,13 @@
 package project.facetunes.facetunes;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
@@ -13,8 +17,9 @@ import android.widget.TableRow;
 
 public class SelectMood extends AppCompatActivity {
 
-    public int[] emojiClickedVal = new int[2];
-    public final int EMOJIS_PER_CATEGORY = 4;
+    public static int[] emojiClickedVal = new int[2];
+    public static final int EMOJIS_PER_CATEGORY = 4;
+    public static final String MOOD_VAL = "Key Value for activity launch on click";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -28,17 +33,31 @@ public class SelectMood extends AppCompatActivity {
         final Button[][] emojiSet =
                 new Button[emojiTable.getChildCount()][EMOJIS_PER_CATEGORY];
 
+        //TODO: gather images and populate this array
+        final Drawable emojisImages[][] = new Drawable[emojiSet.length][emojiSet[0].length];
+
         for(int row = 0; row < emojiSet.length; row++){
             for(int col = 0; col < emojiSet[row].length; col++){
-                Button currentButton = new Button(this);
-                TableRow currentTableRow = (TableRow)(emojiTable.getChildAt(row));
                 final int currentRow = row;
                 final int currentCol = col;
+                TableRow currentTableRow = (TableRow)(emojiTable.getChildAt(row));
+
+                Button currentButton = new Button(this);
+                //sets image
+                currentButton.setBackground(emojisImages[row][col]);
+
+                //sets onClick listener to row, column into an int array
                 currentButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         emojiClickedVal[0] = currentRow;
                         emojiClickedVal[1] = currentCol;
+
+                        //TODO CHANGE TO NEW ACTIVITY WHEN MOOD GETS SELECTED
+                        Intent myIntent = new Intent(SelectMood.this, HomeActivity.class);
+                        myIntent.putExtra(MOOD_VAL, emojiClickedVal);
+                        startActivity(myIntent);
+
                     }
                 });
                 emojiSet[row][col] = currentButton;
