499acaa8cbbad3ce73b7e619f36e05c486bedc0d
Sahil Modi <sm34524@gmail.com>
2017-11-04 15:22:55 -0500

    finished blocked items list activity

--- FaceTunes/app/src/main/java/project/facetunes/facetunes/BlacklistedItems.java
+++ FaceTunes/app/src/main/java/project/facetunes/facetunes/BlacklistedItems.java
@@ -1,76 +1,115 @@
 package project.facetunes.facetunes;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class BlacklistedItems extends AppCompatActivity {
 
     public final static String BLOCKED_ITEMS_LIST_PREF = "KEY FOR BLOCKED ITEMS PREFERENCES";
-    private final static String KEY_GEN_CODE = "KEY GENERATOR PREF";
 
     /**
-     * Type BlacklistedItems.BLOCK_SONG in intent.putStringExtra() to block the song.
+     * Type BlacklistedItems.BLOCK_SONG in intent.putStringExtra() to add song to blocked list.
      */
     public final static String BLOCK_SONG = "STRING TO BLOCK A SONG";
 
     SharedPreferences blItemsPref;
     SharedPreferences.Editor blItemsPrefEdit;
 
-    SharedPreferences keyPref;
-    SharedPreferences.Editor keyPrefEdit;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blacklisted_items);
 
-        ArrayList<String> blockedItems = new ArrayList<>();
+        final ListView blockedItemsListView = (ListView) findViewById(R.id.blocked_items_list);
 
         blItemsPref = getSharedPreferences(BLOCKED_ITEMS_LIST_PREF, MODE_PRIVATE);
         blItemsPrefEdit = blItemsPref.edit();
 
-        keyPref = getSharedPreferences(KEY_GEN_CODE, MODE_PRIVATE);
-        keyPrefEdit = keyPref.edit();
-
         Intent intent = getIntent();
 
         if (intent.hasExtra(BLOCK_SONG)) {
-            //Generate new key;
-            int key = keyPref.getInt(KEY_GEN_CODE, 0) + 1;
-            String uniqueKey = "Blocked Item #" + key;
-            keyPrefEdit.putInt(KEY_GEN_CODE, key);
-            keyPrefEdit.apply();
-
             String blockedItem = intent.getStringExtra(BLOCK_SONG);
-            blItemsPrefEdit.putString(uniqueKey, blockedItem);
+            blItemsPrefEdit.putString(blockedItem, blockedItem);
             blItemsPrefEdit.apply();
         }
 
-        //Fill ArrayList
-        for(String key : blItemsPref.getAll().keySet()){
-            blockedItems.add(blItemsPref.getString(key, ""));
-        }
+        //FOR TESTING
+//        for (String item : blockedItems) {
+//            Toast.makeText(this, item, Toast.LENGTH_SHORT).show();
+//        }
+//        clearSharedPref();
 
-        for(String item : blockedItems){
-            Toast.makeText(this, item, Toast.LENGTH_SHORT).show();
-        }
+        populateListView(blockedItemsListView);
 
-        //clearSharedPref();
+
+        blockedItemsListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(BlacklistedItems.this);
+                builder.setTitle(R.string.unblock_item_title)
+                        .setPositiveButton(R.string.unblock_item_yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String blockedItemName =
+                                        blockedItemsListView.getItemAtPosition(position).toString();
+                                blItemsPrefEdit.remove(blockedItemName);
+                                blItemsPrefEdit.apply();
+                                populateListView(blockedItemsListView);
+                            }
+                        })
+                        .show();
+
+                return false;
+            }
+        });
 
     }
 
     //FOR TESTING PURPOSES
-    private void clearSharedPref(){
+    private void clearSharedPref() {
         blItemsPrefEdit.clear();
         blItemsPrefEdit.commit();
+    }
 
-        keyPrefEdit.clear();
-        keyPrefEdit.apply();
+    /**
+     * Populate the list view. TODO update to recycler view later on.
+     *
+     * @param lv listview
+     */
+    private void populateListView(ListView lv) {
+        ArrayList<String> aList = new ArrayList<>();
+
+        for (String key : blItemsPref.getAll().keySet()) {
+            //Populates the array list with all values besides the unique key count
+            aList.add(blItemsPref.getString(key, ""));
+        }
+
+        //Alphabetical order sort
+        Collections.sort(aList, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                BlacklistedItems.this,
+                android.R.layout.simple_list_item_1,
+                aList
+        );
+        lv.setAdapter(arrayAdapter);
+
     }
 }
