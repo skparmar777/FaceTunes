d599a234f57744e7803d488d3b4dc208fda27abe
rohinb2 <bhasin.rohin@gmail.com>
2017-12-04 23:20:41 -0600

    Android FaceAsyncTask

---
+++ app/src/main/java/com/example/rohinbhasin/facetunes/Face.java
@@ -0,0 +1,20 @@
+package com.example.rohinbhasin.facetunes;
+
+/**
+ * Created by rohinbhasin on 10/30/17.
+ */
+
+public class Face {
+
+    private FaceRectangle faceRectangle;
+    private Scores scores;
+
+    public FaceRectangle getFaceRectangle() {
+        return faceRectangle;
+    }
+
+    public Scores getScores() {
+        return scores;
+    }
+}
+
